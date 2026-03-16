6ebe0b724de9e1f8e7dcb7e01fb2f98ac3f8bd03
Rafi A Syifaa <mrafiasyifaa@gmail.com>
2026-03-17 04:15:13 +0700

    feat: Polishing LoginForm.tsx & Adding zod

--- my-app/src/components/auth/LoginForm.tsx
+++ my-app/src/components/auth/LoginForm.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+"use client"
+
+import React, {useState} from 'react'
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '../ui/card'
 import Link from 'next/link'
 
 function LoginForm() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) =>{
+    e.preventDefault();
+    setIsLoading(true);
+
+    await new Promise((resolve)=> setTimeout(resolve, 2000));
+
+    setIsLoading(false);
+    alert("Coba Login berhasil!");
+  }
+
   return (
     <Card>
       <CardHeader>
@@ -10,10 +24,10 @@ function LoginForm() {
         <CardDescription>Access your wallet securely</CardDescription>
       </CardHeader>
       <CardContent>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='flex flex-col gap-2'>
             <label htmlFor='email'>Email</label>
-            <input className='border rounded-md border-cassiterite-brown rounded-p-2 focus-within:border-black-slug'
+            <input className='border rounded-md border-cassiterite-brown p-2 focus:border-black-slug'
             type='email'
             name='email'
             id='email' 
@@ -23,7 +37,7 @@ function LoginForm() {
           <div className='flex flex-col gap-2'>
             <label htmlFor='password'>Password</label>
             <input
-            className='border rounded-md border-cassiterite-brown rounded-p-2 focus-within:border-black-slug'
+            className='border rounded-md border-cassiterite-brown rounded-p-2 focus:border-black-slug'
             type='password'
             name='password'
             id='password'
@@ -33,7 +47,11 @@ function LoginForm() {
             <Link href="/forgot-password" className="flex justify-end text-sm items-end text-black-slug hover:text-chill-con-carne hoverEffect">Lupa Password?</Link>
           </div>
 
-          <button className='rounded-md w-full mt-8 bg-black-slug text-white p-2 rounded-p-2 hover:bg-chill-con-carne hoverEffect'>Sign In</button>
+          <button
+          type="submit"
+          disabled={isLoading} 
+        
+          className={`rounded-md w-full mt-8 bg-black-slug text-white p-2 rounded-p-2 hover:bg-chill-con-carne hoverEffect ${isLoading ? 'bg-gray-400 cursor-not-allowed' : 'bg-black'}`}>{isLoading? 'Memasukkan data Anda...' : 'Sign In'}</button>
         </form>
       </CardContent>
 
